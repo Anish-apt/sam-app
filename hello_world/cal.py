2328b5132fecaa737a74c50a2d7143f5d9357bf0
EC2 Default User <ec2-user@ip-172-31-7-134.ap-south-1.compute.internal>
2022-01-18 12:03:55 +0000

    first

--- hello_world/cal.py
+++ hello_world/cal.py
@@ -1,16 +1,36 @@
 def add(num1, num2):
-    return num1 + num2
-
+	return num1 + num2
 
 
 def subtract(num1, num2):
-    return num1 - num2
+	return num1 - num2
 
 
 def multiply(num1, num2):
-    return num1 * num2
-
+	return num1 * num2
 
 
 def divide(num1, num2):
-    return num1 / num2
+	return num1 / num2
+
+def lambda_handler(event, context):
+    
+	op = event['op']
+	number_1 = int(event['a'])
+	number_2 = int(event['b'])
+
+	if op == '+':
+		print(number_1, "+", number_2, "=",
+						add(number_1, number_2))
+
+	elif op == '-':
+		print(number_1, "-", number_2, "=",
+						subtract(number_1, number_2))
+
+	elif op == '*':
+		print(number_1, "*", number_2, "=",
+						multiply(number_1, number_2))
+
+	elif op == '/':
+		print(number_1, "/", number_2, "=",
+						divide(number_1, number_2))
